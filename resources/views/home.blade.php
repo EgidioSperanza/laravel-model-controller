654f6881829dddf2e5f1d7f9c9dfa8e2db0d33bf
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-03-04 17:28:08 +0100

    add star for vote average

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,27 @@
 <div class="d-flex flex-wrap justify-content-center gap-3 my-5">
     @foreach ($movies as $movie)
     {{-- @dump($movie); --}}
-    <div class="card text-center">
+    <div class="card text-center pt-5">
         <h1>{{$movie->title}}</h1>
-        <div class="card-body">
+        <div class="card-body p-5">
+            <p class="card-text">Original Title</p>
             <h5 class="card-title">{{$movie->original_title}}</h5>
-            <p class="card-text">{{$movie->nationality}}</p>
+            <p class="card-text">Nation: {{$movie->nationality}}</p>
             <p class="card-text">{{$movie->date}}</p>
-            <p class="card-text">{{$movie->vote}}</p>
+            <p class="card-text">Vote:
+                @for ( $i=0; $i<5; $i++)
+                    @if ($movie->vote/2 - $i >= 1)
+                    <i class="bi bi-star-fill"></i>
+                    @elseif ($movie->vote/2 - $i > 0 && $movie->vote/2 - $i < 1)
+                    <i class="bi bi-star-half"></i>
+                    @else 
+                    <i class="bi bi-star"></i>
+                    @endif
+                @endfor
+                {{$movie->vote}}
+            </p>
         </div>
     </div>
     @endforeach
 </div>
->@endsection
+@endsection
